1664ca5ff8e359148f259f6727c2b98cbd022dea
Jake Schwarz <js075616@ohio.edu>
2022-04-12 16:18:06 -0400

    Updated tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,18 +3,18 @@ const supertest = require("supertest");
 const requestWithSupertest = supertest(server);
 
 describe("Summon Endpoint", () => {
-  it("GET /api/summon contains card0", async () => {
-    const res = await requestWithSupertest.get("/api/summon");
+  it("GET /api/summon/firstbanner contains card0", async () => {
+    const res = await requestWithSupertest.get("/api/summon/firstbanner");
     expect(res.status).toEqual(200);
     expect(res.type).toEqual(expect.stringContaining("json"));
-    expect(res.body).toHaveProperty("card0");
+    expect(res.body.cards[0]).toHaveProperty("id");
   });
 
-  it("GET /api/summon contains card9", async () => {
-    const res = await requestWithSupertest.get("/api/summon");
+  it("GET /api/summon/firstbanner contains card9", async () => {
+    const res = await requestWithSupertest.get("/api/summon/firstbanner");
     expect(res.status).toEqual(200);
     expect(res.type).toEqual(expect.stringContaining("json"));
-    expect(res.body).toHaveProperty("card9");
+    expect(res.body.cards[9]).toHaveProperty("id");
   });
 
   it("GET /api/ doesn't work", async () => {
